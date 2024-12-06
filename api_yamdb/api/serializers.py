f6a67a05327d12310dd7173cda4f0ef04d0ed7db
Aleksandr Romanov <rzn1nsky@yandex.ru>
2024-12-06 21:32:33 +0300

    fix

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -6,7 +6,7 @@ from reviews.models import Category, Comment, Genre, Review, Title
 
 class CategorySerializer(serializers.ModelSerializer):
     """Сериализатор для категорий."""
-    
+
     class Meta:
         model = Category
         fields = ('name', 'slug')
@@ -15,7 +15,7 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class GenreSerializer(serializers.ModelSerializer):
     """Сериализатор для жанров."""
-    
+
     class Meta:
         model = Genre
         fields = ('name', 'slug')
@@ -24,7 +24,7 @@ class GenreSerializer(serializers.ModelSerializer):
 
 class TitleSerializer(serializers.ModelSerializer):
     """Сериализатор для GET запросов произведений."""
-    
+
     category = CategorySerializer(
         read_only=True
     )
@@ -41,7 +41,7 @@ class TitleSerializer(serializers.ModelSerializer):
 
 class TitlePostSerializer(serializers.ModelSerializer):
     """Сериализатор для POST запросов произведений."""
-    
+
     category = serializers.SlugRelatedField(
         slug_field='slug',
         queryset=Category.objects.all()
@@ -59,7 +59,7 @@ class TitlePostSerializer(serializers.ModelSerializer):
 
 class ReviewSerializer(serializers.ModelSerializer):
     """Сериализатор для POST запросов отзывов."""
-    
+
     author = serializers.SlugRelatedField(
         slug_field='username',
         read_only=True,
@@ -67,13 +67,16 @@ class ReviewSerializer(serializers.ModelSerializer):
     )
     score = serializers.IntegerField()
     title = serializers.PrimaryKeyRelatedField(read_only=True)
-    
+
     def validate(self, data):
         request = self.context['request']
         author = request.user
         title_id = self.context['view'].kwargs.get('title_id')
         if request.method == 'POST':
-            if Review.objects.filter(title_id=title_id, author=author).exists():
+            if Review.objects.filter(
+                title_id=title_id,
+                author=author
+            ).exists():
                 raise ValidationError('Отзыв уже оставлен.')
         score = data.get('score')
         if score is not None and (score < 1 or score > 10):
@@ -87,7 +90,7 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 class CommentSerializer(serializers.ModelSerializer):
     """Сериализатор для POST запросов комментариев."""
-    
+
     author = serializers.SlugRelatedField(
         read_only=True,
         slug_field='username'
