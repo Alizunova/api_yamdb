4e9face13e49a02c71b6b283d45cb4a6136786b9
Aleksandr Romanov <rzn1nsky@yandex.ru>
2024-12-09 22:09:40 +0300

    Fix api
    Co-authored-by: Epine369 <silivin369@mail.ru>

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,46 +3,51 @@ from rest_framework import permissions
 
 class IsAdminModeratorAuthorOrReadOnly(permissions.BasePermission):
     """
-    Пользовательские права администратора, модератора, автора,
-    аутонтифицироанного пользователя.
+    Права доступа для разных пользователей.
+
+    - Безопасные методы (GET, HEAD, OPTIONS) доступны всем.
+    - POST доступен только аутентифицированным пользователям.
+    - PUT и DELETE доступны автору объекта, модератору или администратору.
     """
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.method == 'POST':
-            return request.user.is_authenticated
-        return (request.user.is_authenticated and (
-            request.user == obj.author
-            or request.user.is_moderator
-            or request.user.is_admin
-        ))
+        return (
+            request.method in permissions.SAFE_METHODS
+            or (
+                request.user.is_authenticated and (
+                    request.user == obj.author
+                    or request.user.is_moderator
+                    or request.user.is_admin
+                )
+            )
+        )
 
 
 class IsAdminSuperuser(permissions.BasePermission):
     """
-    Пользовательские права администратора, суперпользователя,
-    аутонтифицироанного пользователя.
+    Права доступа для администратора и суперпользователя.
+
+    Разрешает доступ только аутентифицированным пользователям,
+    которые являются администраторами или суперпользователями.
     """
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
-            request.user.is_admin
-            or request.user.is_superuser
+            request.user.is_admin or request.user.is_superuser
         )
 
 
 class IsAdminUserOrReadOnly(permissions.BasePermission):
     """
-    Пользовательские права администратора, суперпользователя,
-    анонима, аутонтифицироанного пользователя.
+    Права доступа для администраторов и анонимных пользователей.
+
+    - Безопасные методы доступны всем.
+    - Остальные методы доступны только администраторам
+      или суперпользователям.
     """
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
             or (
                 request.user.is_authenticated
-                and (
-                    request.user.is_admin
-                    or request.user.is_superuser
-                )
+                and (request.user.is_admin or request.user.is_superuser)
             )
         )
