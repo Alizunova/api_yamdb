c7600664effe2fbb2e8dc843977ba8dfe7b9af49
Aleksandr Romanov <rzn1nsky@yandex.ru>
2024-12-09 23:05:19 +0300

    Fix

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -14,22 +14,22 @@ class GenreInTabular(admin.TabularInline):
 @admin.register(Category)
 @admin.register(Genre)
 class GenreCategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug',]
+    list_display = ['name', 'slug']
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['review', 'text', 'author', 'pub_date',]
+    list_display = ['review', 'text', 'author', 'pub_date']
 
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ['title', 'score', 'text', 'author', 'pub_date',]
+    list_display = ['title', 'score', 'text', 'author', 'pub_date']
 
 
 @admin.register(Title)
 class TitleAdmin(admin.ModelAdmin):
-    list_display = ['name', 'year', 'get_genre', 'category', 'description',]
+    list_display = ['name', 'year', 'get_genre', 'category', 'description']
     list_editable = ('category',)
     fields = ['name', 'year', 'category', 'description']
     inlines = [GenreInTabular,]
